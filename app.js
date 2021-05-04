2be0b087d6b0d1cba55df3ef95dbf736b5285c52
Yasir <code.yasir@gmail.com>
2021-05-05 01:05:37 +0300

    finish program

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Hello, World!');
+const word1 = ['Always', 'I can', 'I\'ll'];
+const word2 = ['be', 'become', 'act'];
+const word3 = ['better.', 'strong', 'brave'];
+
+let random = Math.floor(Math.random() * 3);
+
+console.log(`${word1[random]} ${word2[random]} ${word3[random]}`);
